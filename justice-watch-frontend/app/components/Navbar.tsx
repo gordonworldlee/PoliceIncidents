326b0747d7aee0346f1f7741518cc167309b00be
Gabriel Keller <gabrieljameskeller@gmail.com>
2025-02-19 00:18:19 -0600

    add logo to navbar

--- justice-watch-frontend/app/components/Navbar.tsx
+++ justice-watch-frontend/app/components/Navbar.tsx
@@ -1,13 +1,27 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Navbar() {
   return (
     <div className="bg-[#FFF4E1] py-4 px-6 flex justify-between items-center">
-      <Link className="text-2xl font-bold text-[#D92552]" href="/">JUSTICEWATCH</Link>
+      <Link className="text-2xl font-bold text-[#D92552]" href="/">
+        <Image
+          src="/logo-32px.png"
+          alt="JusticeWatch"
+          width={251}
+          height={32}
+        />
+      </Link>
       <div className="space-x-6">
-        <Link className="text-black font-medium" href="/department">AGENCIES</Link>
-        <Link className="text-black font-medium" href="/violence">INCIDENTS</Link>
-        <Link className="text-black font-medium" href="/legislation">LEGISLATION</Link>
+        <Link className="text-black font-medium" href="/department">
+          AGENCIES
+        </Link>
+        <Link className="text-black font-medium" href="/violence">
+          INCIDENTS
+        </Link>
+        <Link className="text-black font-medium" href="/legislation">
+          LEGISLATION
+        </Link>
       </div>
     </div>
   );
