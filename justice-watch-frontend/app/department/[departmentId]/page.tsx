59dc7ad77857f8b44ef4c33ed33dec3c1db44268
longphan084 <longphan084@gmail.com>
2025-02-27 19:04:16 -0600

    added map to violence instances

--- justice-watch-frontend/app/department/[departmentId]/page.tsx
+++ justice-watch-frontend/app/department/[departmentId]/page.tsx
@@ -125,65 +125,6 @@ export default async function DepartmentPage({
 }: DepartmentPageProps) {
   const { departmentId } = await params;
   console.log(searchParams);
-  // const departmentInstances: Record<string, DepartmentInstance> = {
-  //   dallas: {
-  //     agency_name: "dallas",
-  //     agency_type: "Police Department",
-  //     location_name: "dallas",
-  //     state: "TEXAS",
-  //     ori: "TXDPD0000",
-  //     latitude: 32.76778,
-  //     longitude: -96.79468,
-  //     total_population: 1278608,
-  //     calc_police_funding_score: 38,
-  //     use_of_force_reported: 268,
-  //     calc_police_violence_score: 48,
-  //     police_shooting_avg: 4.3,
-  //     calc_police_accountability_score: 49,
-  //     overall_score: 46,
-  //     incident_id: "incident3",
-  //     dept_website: "https://dallaspolice.net/home",
-  //     department_image: "/dallasPD.png"
-  //   },
-  //   houston: {
-  //     agency_name: "houston",
-  //     agency_type: "Police Department",
-  //     location_name: "houston",
-  //     state: "TEXAS",
-  //     ori: "TXHPD0000",
-  //     latitude: 29.96826,
-  //     longitude: -95.36137,
-  //     total_population: 2297580,
-  //     calc_police_funding_score: 52,
-  //     use_of_force_reported: 229,
-  //     calc_police_violence_score: 42,
-  //     police_shooting_avg: 3.2,
-  //     calc_police_accountability_score: 45,
-  //     overall_score: 47,
-  //     incident_id: "incident1",
-  //     dept_website: "https://www.houstontx.gov/police/",
-  //     department_image: "/houstonPD.png"
-  //   },
-  //   austin: {
-  //     agency_name: "austin",
-  //     agency_type: "Police Department",
-  //     location_name: "austin",
-  //     state: "TEXAS",
-  //     ori: "TX2270100",
-  //     latitude: 30.26993,
-  //     longitude: -97.74315,
-  //     total_population: 943059,
-  //     calc_police_funding_score: 41,
-  //     use_of_force_reported: 301,
-  //     calc_police_violence_score: 40,
-  //     police_shooting_avg: 3.5,
-  //     calc_police_accountability_score: 4,
-  //     overall_score: 32,
-  //     incident_id: "incident2",
-  //     dept_website: "https://www.austintexas.gov/department/police",
-  //     department_image: "/austinPD.jpeg"
-  //   },
-  // };
 
   const departmentInstance = DepartmentInstances[departmentId];
   const departmentName =
@@ -203,8 +144,8 @@ export default async function DepartmentPage({
         <h1 className="my-8 text-center text-3xl font-bold">
           {departmentName} Police Department
         </h1>
-        <div className="w-48 h-48">
-          <img  src={departmentInstance.department_image} className="w-full h-full object-contain" alt="outline of Texas State" />
+        <div className="flex flex-row justify-center">
+          <img className="w-48 h-48 rounded-lg" src={departmentInstance.department_image} alt="outline of Texas State" />
         </div>
         <div>
           <div className="text-left border-b-2 border-gray-300 rounded-lg shadow-md p-4 bg-white">
@@ -258,7 +199,7 @@ export default async function DepartmentPage({
                 </p>
               </div>
               <div className="mr-4 w-1/2">
-                  <Map department={departmentInstance}/>
+                  <Map latitude={departmentInstance.latitude} longitude={departmentInstance.longitude}/>
               </div>
             </div>
             <div>
