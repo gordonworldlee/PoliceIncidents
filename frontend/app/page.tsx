4723365db2ea68f88306c26793846a78ba0c006e
longphan084 <longphan084@gmail.com>
2025-03-04 21:02:51 -0600

    gabe pls check

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -9,11 +9,25 @@ export default function Home() {
 
       {/* Main Content */}
       <div className="max-w-3xl mx-auto mt-16 text-left px-4">
-        <h1 className="text-3xl font-bold ">
+        <h1 className="text-3xl font-bold">
           Uphold police accountability, one{" "}
-          <span className="text-[#D92552]">agency</span>, piece of{" "}
-          <span className="text-[#D92552]">legislation</span>, and{" "}
-          <span className="text-[#D92552]">incident</span> at a time.
+          <Link className="relative inline-block group" href="/department">
+            <span className="inline-block text-[#D92552]">agency</span>
+            <span className="absolute left-1/2 bottom-0 h-[2px] w-0 bg-[#D92552] transition-all duration-300 ease-out group-hover:w-full transform -translate-x-1/2"></span>
+          </Link>
+          <span>, piece of</span>
+          <Link className="relative inline-block group" href="/legislation">
+            <span className="inline-block text-[#D92552]">legislation</span>
+            <span className="absolute left-1/2 bottom-0 h-[2px] w-0 bg-[#D92552] transition-all duration-300 ease-out group-hover:w-full transform -translate-x-1/2"></span>
+          </Link>
+          <span>, and </span>
+          <Link href="/incident" className="relative inline-block group">
+            <span className="relative z-10 text-[#D92552] transition-transform duration-300 ease-out group-hover:-translate-y-1.5"> incident </span>
+            <span className="absolute left-1/2 bottom-0 h-[2px] w-0 bg-[#D92552] transition-all duration-300 ease-out group-hover:w-full transform -translate-x-1/2"></span>
+          </Link>
+          <span> at a time.</span>
+
+
         </h1>
         <p className="mt-4 text-center mt-14">
           Search across thousands of U.S. records, or visit your city&apos;s
@@ -62,13 +76,13 @@ export default function Home() {
         </div>
       </div>
 
-      {/* Make it scrollable */}
+      {/* Make it scrollable
       <div className="max-w-3xl mx-auto mt-12 px-4 pb-32">
         <h2 className="text-lg font-semibold mb-4"></h2>
         {[...Array(20)].map((_, i) => (
           <div key={i} className="bg-white p-4 mb-2 rounded"></div>
         ))}
-      </div>
+      </div> */}
 
       {/* Footer Links */}
       <div className="max-w-3xl mx-auto mt-16 text-left px-4 py-8">
