f5866d15bbe27ec718a1ec961b80a31309f5ef53
Gordon <gordonworldlee@utexas.edu>
2025-04-16 18:02:09 -0500

    fixing nav bard to remove search

--- frontend/app/components/Navbar.tsx
+++ frontend/app/components/Navbar.tsx
@@ -38,7 +38,6 @@ export default function Navbar() {
           <Link className="text-black font-medium" href="/department">AGENCIES</Link>
           <Link className="text-black font-medium" href="/violence">INCIDENTS</Link>
           <Link className="text-black font-medium" href="/legislation">LEGISLATION</Link>
-          <Link className="text-black font-medium" href="/search">SEARCH</Link>
           <Link className="text-black font-medium" href="/about">ABOUT US</Link>
         </div>
       </div>
@@ -49,7 +48,6 @@ export default function Navbar() {
           <Link className="block text-black font-medium" href="/department">AGENCIES</Link>
           <Link className="block text-black font-medium" href="/violence">INCIDENTS</Link>
           <Link className="block text-black font-medium" href="/legislation">LEGISLATION</Link>
-          <Link className="block text-black font-medium" href="/search">SEARCH</Link>
           <Link className="block text-black font-medium" href="/about">ABOUT US</Link>
         </div>
       )}
